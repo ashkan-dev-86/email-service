eb27a73899f92d675fa6ea6317fca30710e93ea5
Ashkan <ashkan65@hotmail.com>
2021-08-27 18:43:31 +0800

    apply new email

--- src/configs.js
+++ src/configs.js
@@ -14,13 +14,13 @@ const config = {
     SECRET: "secretkey",
     EMAIL_HOST: "smtp.elasticemail.com",
     EMAIL_PORT: 2525,
-    EMAIL_USER: "ashkan.aliabbasi@gmail.com",
-    EMAIL_PASS: "60CA9189EB9AFAF68B886115D1F90638069E",
+    EMAIL_USER: "ashkanred86@gmail.com",
+    EMAIL_PASS: "5D256A1DE7744FE17032B03B90D6B65B76E4",
     EMAIL_DEFAULT_SUBJECT: "Email from a email delivery service",
     EMAIL_DEFAULT_MARKUP: "<b>Hello receiver</b>",
     EMAIL_KEY:
       "2AE6F24DE714621978F9793734E1143738700497E6B29BBB52A56982B774AE411C5238D17C0069043559B1482F25CDF5",
-    EMAIL_SENDER: '"Email Service" <ashkan.aliabbasi@gmail.com>',
+    EMAIL_SENDER: '"Email Service" <ashkanred86@gmail.com>',
   },
 };
 
